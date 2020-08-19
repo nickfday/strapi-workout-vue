073a4f7439184791e9c7245ba9049866740400bc
Nicholas Finley-Day <nickfday@gmail.com>
2020-08-19 19:13:58 +0100

    project scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,61 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <div>
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+          <b-navbar-brand href="/">Workout Mentor</b-navbar-brand>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+              <b-nav-item href="/exercises">Exercises</b-nav-item>
+              <b-nav-item href="/routines">Routines</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+              <b-nav-form>
+                <b-form-input
+                  size="sm"
+                  class="mr-sm-2"
+                  placeholder="Search"
+                ></b-form-input>
+                <b-button size="sm" class="my-2 my-sm-0" type="submit"
+                  >Search</b-button
+                >
+              </b-nav-form>
+
+              <b-nav-item-dropdown text="Lang" right>
+                <b-dropdown-item href="#">EN</b-dropdown-item>
+                <b-dropdown-item href="#">ES</b-dropdown-item>
+                <b-dropdown-item href="#">RU</b-dropdown-item>
+                <b-dropdown-item href="#">FA</b-dropdown-item>
+              </b-nav-item-dropdown>
+
+              <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template v-slot:button-content>
+                  <em>User</em>
+                </template>
+                <b-dropdown-item href="/athlete/dashboard"
+                  >Dashboard</b-dropdown-item
+                >
+                <b-dropdown-item href="/auth/register"
+                  >Register</b-dropdown-item
+                >
+                <b-dropdown-item href="/auth/login">Login</b-dropdown-item>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-navbar>
+      </div>
+      <b-container class="bv-example-row">
+        <b-row>
+          <br />
+          <br />
+          <nuxt />
+        </b-row>
+      </b-container>
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,35 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Exercises from '../views/exercises';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'Home',
+    component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/exercises',
+    name: 'exercises',
+    component: Exercises,
+  },
 ];
 
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  routes,
 });
 
 export default router;
