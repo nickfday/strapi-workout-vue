81d0563d20339d6902c6f1fe0d5e0d67e703ce6b
Nicholas Finley-day <nickf@Nick-MBP-2015.lan>
2020-08-23 22:37:49 +0100

    header changes

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar dark>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-      <v-toolbar-title>Workout Mentor</v-toolbar-title>
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-menu left bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon v-bind="attrs" v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav>
-        <v-list-item-group v-model="group" active-class="text--accent-4">
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+    <Header />
 
     <v-main>
       <router-view />
@@ -54,8 +9,12 @@
 </template>
 
 <script>
+import Header from '@/components/Header';
 export default {
   name: 'App',
+  components: {
+    Header
+  },
 
   data: () => ({
     drawer: false
---
+++ src/components/Header/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <v-app-bar dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Workout Mentor</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <router-link
+            :to="item.slug"
+            v-for="item in userMenuComputed"
+            :key="item.title"
+          >
+            <v-list-item>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </router-link>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav>
+        <v-list-item-group v-model="group" active-class="text--accent-4">
+          <router-link
+            :to="item.slug"
+            v-for="item in navItems"
+            :key="item.title"
+          >
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item></router-link
+          >
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  data: () => ({
+    drawer: false,
+    navItems: [
+      {
+        title: 'Home',
+        slug: '/',
+        icon: 'mdi-home'
+      },
+      {
+        title: 'Exercises',
+        slug: '/exercises',
+        icon: 'mdi-arm-flex'
+      }
+    ],
+    userMenu: [
+      {
+        title: 'Log In',
+        slug: '/auth/login'
+      },
+      {
+        title: 'Register',
+        slug: '/auth/register'
+      },
+      {
+        title: 'Dashboard',
+        slug: '/athlete/dashboard',
+        auth: true
+      }
+    ]
+  }),
+  computed: {
+    userMenuComputed: function() {
+      return this.userMenu.filter((item) => {
+        return this.$store.getters.loggedIn ? item.auth : !item.auth;
+      });
+    }
+  }
+};
+</script>
