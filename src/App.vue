623523196bcaef14d830ea030b712cfce8a9181c
Nicholas Finley-Day <nickfday@gmail.com>
2020-08-23 20:53:18 +0100

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,64 @@
 <template>
-  <div id="app">
-    <Header />
-    <b-container>
+  <v-app>
+    <v-app-bar dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Workout Mentor</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav>
+        <v-list-item-group v-model="group" active-class="text--accent-4">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
       <router-view />
-    </b-container>
-  </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Header from '@/layout/header';
 export default {
-  components: {
-    Header
-  }
+  name: 'App',
+
+  data: () => ({
+    drawer: false
+  })
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'nprogress/nprogress.css';
 import '@/assets/css/style.css';
+import vuetify from './plugins/vuetify';
 
+//TODO: Remove bootstrap
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
@@ -16,6 +18,7 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+
   created() {
     const userString = localStorage.getItem('user');
     if (userString) {
@@ -23,5 +26,7 @@ new Vue({
       this.$store.commit('SET_USER_DATA', userData);
     }
   },
+
+  vuetify,
   render: (h) => h(App)
 }).$mount('#app');
