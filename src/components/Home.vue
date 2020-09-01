9640702f071c5edbd8b3d90ec49bc09f45934f4f
Nicholas Finley-Day <nickfday@gmail.com>
2020-09-01 23:14:34 +0100

    clean up

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,38 +15,12 @@
             <v-card-title>{{ card.title }}</v-card-title>
           </v-img>
 
-          <!-- <v-card-subtitle class="pb-0">Number 10</v-card-subtitle> -->
-
           <v-card-text class="text--primary">
             <div>{{ card.body }}</div>
           </v-card-text>
         </v-card>
       </v-col>
     </v-row>
-
-    <!-- <b-row>
-      <b-col
-        v-for="card in pageData.components[1].cardGroup"
-        v-bind:key="card.title"
-        sm="6"
-      >
-        <b-card
-          :title="`${card.title}`"
-          :img-src="`${card.media.url}`"
-          img-alt="Image"
-          img-top
-          tag="article"
-          class="mb-2"
-        >
-          <b-card-text>{{ card.body }}</b-card-text>
-          <router-link :to="{ path: card.linkSlug }" v-if="card.linkSlug">
-            <b-button variant="primary">
-              {{ card.linkTitle }}
-            </b-button>
-          </router-link>
-        </b-card>
-      </b-col>
-    </b-row> -->
   </div>
 </template>
 
