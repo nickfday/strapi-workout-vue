55d36f3b90d6274c995fc72700339f7420b740ec
Nicholas Finley-Day <nickfday@gmail.com>
2020-09-02 14:22:56 +0100

    replace bootstrap

--- src/views/exercises/exerciseDetail.vue
+++ src/views/exercises/exerciseDetail.vue
@@ -1,40 +1,139 @@
 <template>
   <div>
     <h1>{{ exercise.title }}</h1>
-    <b-row>
-      <b-col sm="8">
-        <b-embed
-          type="iframe"
-          aspect="16by9"
+    <v-row>
+      <v-col sm="8">
+        <iframe
+          width="560"
+          height="315"
           :src="exercise.videoUrl"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
-        ></b-embed>
-      </b-col>
-      <b-col sm="4">
-        <div>
+        ></iframe>
+      </v-col>
+      <v-col sm="4">
+        <v-list>
+          <v-list-item-group color="primary">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Type</v-list-item-title>
+                <v-list-item-subtitle>
+                  {{ exercise.type.replace(/_/g, ' ') }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Primary Muscle</v-list-item-title>
+                <v-list-item-subtitle>
+                  {{ exercise.primaryMuscle.title }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item v-if="exercise.secondaryMuscle.length">
+              <v-list-item-icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Secondary Muscles</v-list-item-title>
+                <v-list-item-subtitle>
+                  <span
+                    v-for="(item, index) in exercise.secondaryMuscle"
+                    v-bind:key="item.id"
+                  >
+                    {{ item.title
+                    }}<span v-if="index != exercise.secondaryMuscle.length - 1"
+                      >,</span
+                    ></span
+                  >
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Equipment</v-list-item-title>
+                <v-list-item-subtitle>
+                  <span
+                    v-for="(item, index) in exercise.equipment"
+                    v-bind:key="item.id"
+                  >
+                    {{ item.title
+                    }}<span v-if="index != exercise.equipment.length - 1"
+                      >,</span
+                    >
+                  </span>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Variation Exercise</v-list-item-title>
+                <v-list-item-subtitle>
+                  <span
+                    v-for="(item, index) in exercise.variationExercise"
+                    v-bind:key="item.id"
+                  >
+                    {{ item.title }}
+                    <span v-if="index != exercise.variationExercise.length - 1">
+                      ,
+                    </span>
+                  </span>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+
+        <!-- <div>
           <b-list-group>
             <b-list-group-item class="d-flex align-items-center">
               <b-avatar class="mr-3"></b-avatar>
               <span class="mr-auto">{{ exercise.type }}</span>
             </b-list-group-item>
+
             <b-list-group-item class="d-flex align-items-center">
               <b-avatar variant="primary" text="BV" class="mr-3"></b-avatar>
               <span class="mr-auto">{{ exercise.primaryMuscle.title }}</span>
             </b-list-group-item>
-            <b-list-group-item class="d-flex align-items-center">
+
+            <b-list-group-item
+              class="d-flex align-items-center"
+              v-if="exercise.secondaryMuscle.length"
+            >
               <b-avatar
                 variant="info"
                 src="https://placekitten.com/300/300"
                 class="mr-3"
               ></b-avatar>
-              <!-- <span v-for="(item, index) in exercise.secondaryMuscle">
+              <span v-for="(item, index) in exercise.secondaryMuscle">
                 {{ item.title }}
                 <span v-if="index != exercise.secondaryMuscle.length - 1"
                   >,</span
                 >
-              </span> -->
+              </span>
             </b-list-group-item>
-            <b-list-group-item class="d-flex align-items-center">
+
+            <b-list-group-item
+              class="d-flex align-items-center"
+              v-if="this.exercise.equipment.length"
+            >
               <b-avatar
                 variant="success"
                 icon="people-fill"
@@ -44,7 +143,11 @@
                 this.exercise.equipment[0].title
               }}</span>
             </b-list-group-item>
-            <b-list-group-item class="d-flex align-items-center">
+
+            <b-list-group-item
+              class="d-flex align-items-center"
+              v-if="exercise.variationExercise.length"
+            >
               <b-avatar
                 variant="success"
                 icon="people-fill"
@@ -55,9 +158,9 @@
               </span>
             </b-list-group-item>
           </b-list-group>
-        </div>
-      </b-col>
-    </b-row>
+        </div> -->
+      </v-col>
+    </v-row>
 
     <vue-simple-markdown :source="exercise.body"></vue-simple-markdown>
   </div>
