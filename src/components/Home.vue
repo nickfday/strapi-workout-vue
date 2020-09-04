f8ebec92a9963e9c3136b8b67733585b9c1837b2
Nicholas Finley-Day <nickfday@gmail.com>
2020-09-04 21:50:36 +0100

    ctabox comp

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
+    <v-carousel hide-delimiters>
+      <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src">
+        <v-row class="fill-height" align="center" justify="center">
+          <!-- <div class="display-3">
+            {{ item.heading }}
+          </div> -->
+          <ctaBox
+            :heading="item.heading"
+            :body="item.body"
+            ctaText=""
+            ctaUrl=""
+            fullWidth="true"
+            padding="30px"
+          />
+        </v-row>
+      </v-carousel-item>
+    </v-carousel>
+
     <v-row>
       <v-col
         v-for="card in pageData.components[1].cardGroup"
@@ -21,15 +39,58 @@
         </v-card>
       </v-col>
     </v-row>
+
+    <v-parallax
+      dark
+      src="https://strapi-workout.s3.eu-west-2.amazonaws.com/pexels-freestocksorg-892757_48d3968168.jpeg"
+      height="400"
+    >
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="display-1 font-weight-thin mb-4">App coming soon!</h1>
+          <ctaBox
+            heading="App coming soon!"
+            body=""
+            ctaText=""
+            ctaUrl=""
+            fullWidth="true"
+          />
+          <!-- <h4 class="subheading">Build your application today!</h4> -->
+        </v-col>
+      </v-row>
+    </v-parallax>
   </div>
 </template>
 
 <script>
 import fetchService from '@/services/fetchService';
 
+import CtaBox from '@/components/ctaBox';
+
 export default {
+  components: {
+    CtaBox
+  },
   data() {
     return {
+      items: [
+        {
+          src:
+            'https://strapi-workout.s3.eu-west-2.amazonaws.com/sunset-jogger_2529c47069.jpeg',
+          heading: 'Our Mission',
+          body:
+            'We believe in encouraging an active a life-style for people of all abilities. We aim to provide a trustworthy resource to help  achieve your fitness ongoing fitness objective.'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+        }
+      ],
       pageData: []
     };
   },
@@ -48,4 +109,13 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.textbox {
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  padding: 15px 30px;
+  position: absolute;
+  bottom: 0;
+  font-size: 18px;
+}
+</style>
---
+++ src/components/ctaBox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cta-box" v-bind:class="{ fullWidth: fullWidth }" :style="cssVars">
+    <h2 p if="heading">{{ heading }}</h2>
+    <p if="body">{{ body }}</p>
+    <a if="ctaText" :href="ctaUrl"> {{ ctaText }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    cssVars() {
+      return { '--padding': this.padding };
+    }
+  },
+  props: ['heading', 'body', 'ctaText', 'ctaUrl', 'fullWidth', 'padding']
+};
+</script>
+
+<style>
+.cta-box {
+  background: rgba(0, 0, 0, 0.5);
+  display: inline-block;
+  padding: var(--padding);
+}
+.fullWidth {
+  display: block;
+  width: 100%;
+}
+</style>
